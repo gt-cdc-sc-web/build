62eff205c08647c0f0b023298e9fb80b09afe751
Sangwhan Moon <sangwhan@iki.fi>
2016-05-01 15:05:44 +0900

    Genesis.

---
+++ js/load.js
@@ -0,0 +1,1 @@
+function call_load_functions_js(){gc_app_js(GC,jQuery)}
---
+++ scripts/refreq.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# this script currently refreshes the ReferralRequest status to 'requested' for testing purposes
+
+server="http://52.72.172.54:8080/fhir/baseDstu2"
+number="19179006"
+#number="5721fc4d0cf20e9addb273b9"
+postnumber=$number
+#server="https://fhir-open-api-dstu2.smarthealthit.org"
+#number="newrefreq"
+#postnumber=""
+
+curl -X PUT --data-binary @$number.json -H 'Content-Type: application/json' $server/ReferralRequest/$postnumber
